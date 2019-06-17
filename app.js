6dc6b3c46c38479f0ec1a854b7bb2506c3a7008c
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-17 10:33:51 +0100

    Working interface

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express  = require('express');
 const cors = require('cors')
 const app = express();
+const bodyParser = require('body-parser');
+const mathRoutes = require('./math');
 
+//configuration of the app
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+//routes
+app.use('/',mathRoutes);
 app.listen(3000,()=>console.log('app running at 3000'));
