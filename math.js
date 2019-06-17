b3dcc6e2318e035ceb0259ef03af6de191cedda8
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-17 11:11:01 +0100

    tweaked the math routes

--- math.js
+++ math.js
@@ -1,18 +1,32 @@
 const router = require('express').Router();
+const db = require('./db');
 
 
-router.get('/',(req,res)=>{
-    res.sendFile(process.cwd() + '/views/index.html');
+router.get('/easy', async (req, res, next) => {
+    try {
+        let easyProblems = db.getEasyProblem((err, founded) => {
+            if (err) next(err);
+            else {
+                res.json(founded);
+            }
+        });
+    } catch (err) {
+        console.log('error api/easy', err)
+    }
 })
 
-//
-router.get('/api/easy',(req,res)=>{
-    res.send('sending an easy problem');
+router.get('/hard', async (req, res, next) => {
+    try {
+        let easyProblems = db.getHardProblem((err, founded) => {
+            if (err) next(err);
+            else {
+                res.json(founded);
+            }
+        });
+    } catch (err) {
+        console.log('error api/hard', err)
+    }
 })
 
-router.get('/api/hard',(req,res)=>{
-    res.send('sending a hard problem');
-})
 
-
-module.exports = router;
+module.exports = router
