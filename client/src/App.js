c26f02f35d3969b353520ad2cc36121f715c8d82
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-17 11:53:09 +0100

    Connected to the server and got the data. Success!

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,42 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
+
 export class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      loading: true,
+      problems:[],
+    }
+  }
+
+  getProblems = async (type) => {
+    try {
+      const problems = await axios.get('https://smed-masarat.glitch.me/api/'+type);
+      console.log('resolved', problems.data);
+      this.setState({
+        loading:false,
+        problems:[...problems.data]
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  } 
+
+componentDidMount(){
+  this.getProblems('easy');
+}
+
   render() {
+    const problems = this.state.problems;
+    console.log(problems);
     return (
       <div>
-        
+        <ul>
+          {this.state.loading && 'DATA IS LOADING'}
+          {!this.state.loading && 'I have the problems now'}
+        </ul>
       </div>
     )
   }
